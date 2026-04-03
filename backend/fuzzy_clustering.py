5a039b35f02c287610bfb26069cd4671b531cd9c
Surya Vamsi <suryavamsigara2@gmail.com>
2026-04-03 20:31:36 +0000

    Create categories end point and wrap clustering in a class

--- backend/fuzzy_clustering.py
+++ backend/fuzzy_clustering.py
@@ -1,83 +1,94 @@
-import numpy as np
 import umap
-import faiss
-import pickle
 import skfuzzy as fuzz
-from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 from build_vector_db import SemanticSearch
 from collections import Counter
 
-search = SemanticSearch(tar_path="")
-
-search.load("db")
-
-num_vectors = search.index.ntotal
-
-embeddings = search.index.reconstruct_n(0, num_vectors)
-print(f"Extracted embeddings shape: {embeddings.shape}")
-
-print("\nReducing dimensions with UMAP...")
-reducer = umap.UMAP(n_neighbors=15, n_components=15, metric='cosine', random_state=42)
-reduced_embeddings = reducer.fit_transform(embeddings)
-
-# Fuzzy C - Means Clustering
-print(f"\nApplying Fuzzy C-Means...")
-data_for_fcm = reduced_embeddings.T
-
-n_clusters = 20
-m_fuzziness = 2.0
-
-# Run FCM
-
-cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
-    data_for_fcm,
-    c=n_clusters,
-    m=m_fuzziness,
-    error=0.005,
-    maxiter=1000,
-    init=None
-)
-
-print(f"FCM converged in {p} iterations. FPC: {fpc:.3f}")
-
-# Transpose back so rows are documents, columns are cluster probabilities
-document_memberships = u.T 
-print(f"Final membership matrix shape: {document_memberships.shape}")
-
-# Loading the saved documents
-with open("db/documents.pkl", "rb") as f:
-    documents = pickle.load(f)
-
-true_labels = [doc['category'] for doc in documents]
-
-print("\nMapping fuzzy clusters to true labels\n")
-primary_cluster_assignments = np.argmax(document_memberships, axis=1)
-
-cluster_names = {}
-
-for cluster_id in range(20):
-    # Docs where this cluster is primary assignment
-    doc_indices_in_cluster = np.where(primary_cluster_assignments == cluster_id)[0]
+class FuzzyClustering:
+    def __init__(self, search: SemanticSearch):
+        self.index = search.index
+        self.documents = search.documents
+        self.document_memberships = None
+        self.cluster_names = {}
     
-    if len(doc_indices_in_cluster) == 0:
-        cluster_names[cluster_id] == "Empty Cluster"
-        continue
+    def run_fcm(self):
+        if self.index is None:
+            print("Index is none.")
+            return
+        
+        num_vectors = self.index.ntotal
+        embeddings = self.index.reconstruct_n(0, num_vectors)
+        print(f"Extracted embeddings shape: {embeddings.shape}")
+        
+        print("\nReducing dimensions with UMAP...")
+        reducer = umap.UMAP(n_neighbors=30, n_components=15, min_dist=0.0, metric='cosine', random_state=42)
+        reduced_embeddings = reducer.fit_transform(embeddings)
 
-    labels_in_cluster = [true_labels[i] for i in doc_indices_in_cluster]
+        # Fuzzy C-Means Clustering
+        print(f"\nApplying Fuzzy C-Means...")
+        data_for_fcm = reduced_embeddings.T
 
-    label_counts = Counter(labels_in_cluster)
-    most_common_label, count = label_counts.most_common(1)[0]
+        n_clusters = 20
+        m_fuzziness = 2.0
 
-    percentage = count / len(doc_indices_in_cluster) * 100
+        # Run FCM
 
-    cluster_names[cluster_id] = most_common_label
+        cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+            data_for_fcm,
+            c=n_clusters,
+            m=m_fuzziness,
+            error=0.005,
+            maxiter=1000,
+            init=None
+        )
 
-print(cluster_names)
+        print(f"FCM converged in {p} iterations. FPC: {fpc:.3f}")
 
-max_probabilities = np.max(document_memberships, axis=1)
+        # Transpose back so rows are documents, columns are cluster probabilities
+        self.document_memberships = u.T 
+        print(f"Final membership matrix shape: {self.document_memberships.shape}")
 
-# Find indices of docs that are shared across topics
-fuzzy_threshold = 0.4
-fuzzy_doc_indices = np.where(max_probabilities < fuzzy_threshold)[0]
+        max_probabilities = np.max(self.document_memberships, axis=1)
 
-print(f"Found {len(fuzzy_doc_indices)} highly fuzzy documents without a strong single topic.")
+        # Find indices of docs that are shared across different labels
+        fuzzy_threshold = 0.4
+        fuzzy_doc_indices = np.where(max_probabilities < fuzzy_threshold)[0]
+        print(f"Found {len(fuzzy_doc_indices)} highly fuzzy documents without a strong single topic.")
+
+
+        true_labels = [doc['category'] for doc in self.documents]
+
+        print("\nMapping fuzzy clusters to true labels\n")
+
+        primary_cluster_assignments = np.argmax(self.document_memberships, axis=1)
+
+        for cluster_id in range(20):
+            doc_indices_in_cluster = np.where(primary_cluster_assignments == cluster_id)[0]
+
+            if len(doc_indices_in_cluster) == 0:
+                self.cluster_names[cluster_id] = "Empty Cluster"
+                continue
+
+            labels_in_cluster = [true_labels[i] for i in doc_indices_in_cluster]
+
+            label_counts = Counter(labels_in_cluster)
+            most_common_label, count = label_counts.most_common(1)[0]
+
+            self.cluster_names[cluster_id] = most_common_label
+        print("Finished")
+    
+    def find_categories(self, index: int):
+        document_array = self.document_memberships[index]
+
+        top_3_cluster_indices = np.argsort(document_array)[::-1][:3].flatten()
+        return {
+            "clusters": [self.cluster_names.get(c) for c in top_3_cluster_indices]
+        }
+
+
+if __name__=="__main__":
+    search = SemanticSearch(tar_path="")
+    search.load("db")
+    clustering = FuzzyClustering(search)
+    clustering.run_fcm()
+
--- backend/main.py
+++ backend/main.py
@@ -4,17 +4,23 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
 from build_vector_db import SemanticSearch
+from fuzzy_clustering import FuzzyClustering
 
 class SearchRequest(BaseModel):
     query: str
+
+class DocRequest(BaseModel):
+    doc: str
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Loading saved index...")
     search = SemanticSearch(tar_path="")
     search.load("db")
-
+    clustering = FuzzyClustering(search=search)
+    clustering.run_fcm()
     app.state.search = search
+    app.state.clustering = clustering
 
     print("✅ Ready!")
     yield
@@ -34,15 +40,21 @@ app.add_middleware(
 def get_search(request: Request):
     return request.app.state.search
 
+def get_clustering(request: Request):
+    return request.app.state.clustering
+
 @app.post("/search")
 async def search_api(body: SearchRequest, search=Depends(get_search)):
     return search.search(body.query)
 
-def getCategories(doc: str, search=Depends(get_search)):
+@app.post("/categories")
+async def get_categories(doc: DocRequest, search=Depends(get_search), clustering=Depends(get_clustering)):
     documents = search.documents
-    index = next((i for i, d in enumerate(documents) if d.get("filename") == doc), None)
+    index = next((i for i, d in enumerate(documents) if d.get("filename") == doc.doc), None)
 
-    
+    if index is None:
+        return {"error": f"Document {doc.doc} not found."}
+    return clustering.find_categories(index)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
